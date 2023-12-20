370b809b26964f846608bbf2ac091714d116c248
Andrés Crespo <acrerosj@canariaseducacion.es>
2023-12-20 20:11:39 +0000

    router and controllers

---
+++ src/Controllers/CarController.php
@@ -0,0 +1,18 @@
+<?php
+namespace Andres\Tema6\Controllers;
+
+class CarController {
+  
+  public function index() {
+    echo "Esto es index de CarController";
+  }
+
+  public function show() {
+    echo "Esto es show de CarController";
+  }
+
+  public function showCarClient($params) {
+    $id = $params['id'];
+    echo "Muestra todos los coches del cliente $id";
+  }
+}
---
+++ src/Controllers/ClientController.php
@@ -0,0 +1,22 @@
+<?php
+namespace Andres\Tema6\Controllers;
+
+class ClientController {
+  private $router;
+
+  public function __construct($router)
+  {
+    $this->router=$router;  
+  }  
+
+  public function index() {
+    echo "Esto es index de ClientController";
+  }
+
+  public function show($params) {
+    $id = $params['id'];
+    echo "<p>Mostrando el cliente con el id: $id</p>";
+    $url = $this->router->generate('car_client_show', ['id' => $id]);
+    echo "<p><a href=\"$url\">Muestra los coches del usuario</a></p>";
+  }
+}
